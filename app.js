55c1afc6513d5981b5f03f70962b75e363203049
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2022-03-15 21:18:54 +0600

    Set security HTTP Headers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import rateLimit from "express-rate-limit";
+import helmet from "helmet";
 import morgan from "morgan";
 
 import tourRouter from "./routes/tourRoutes.js";
@@ -12,11 +13,16 @@ import globalErrorHandler from "./middlewareFunctions/globalErrorHandler.js";
 const app = express();
 
 // GLOBAL MIDDLEWARES
+
+// SET HTTP Headers
+app.use(helmet());
+
+// DEvelopment Logging
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-// Prevent DOS and BRUTE FORCE attacks by rate limitting
+// Prevent DOS and BRUTE FORCE attacks by rate limitting------ Starts
 const limiterFunc = (min, reqNumber, message) => {
   return rateLimit({
     windowMs: min * 60 * 1000, // {min} minutes
@@ -32,9 +38,12 @@ app.use("/api/v1/tours", limiterFunc(20, 600, "Too many request from this IP..Tr
 app.use("/api/v1/users", limiterFunc(15, 200, "Too many request from this IP. Try again 15 min later"));
 app.use("/api/v1/users/login", limiterFunc(5, 20, "Too many logging request from this IP. Try again 5 min later"));
 app.use("/api/v1/users/signup", limiterFunc(10, 25, "Too many account creation request from this IP. Try again 10 min later"));
+// Prevent DOS and BRUTE FORCE attacks by rate limitting------ Ends
 
-app.use(express.json());
+// Body parser, Reading data from body to req.body
+app.use(express.json({ limit: "10kb" })); //Won't accept body that is in size more than 10kb
 
+// Test Middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
 
