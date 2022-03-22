67633bbeba0d5740ac6851abebb5a50d34edbdc6
farhanahmedhasan <achasandjmamunxyz2015@gmail.com>
2022-03-22 23:48:13 +0600

    Added Child Reference

--- models/tourModel.js
+++ models/tourModel.js
@@ -124,6 +124,13 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+      },
+    ],
   },
   {
     toJSON: { virtuals: true },
@@ -145,6 +152,14 @@ tourSchema.pre("save", function (next) {
 tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
   this.start = Date.now();
+  next();
+});
+
+tourSchema.pre(/^find/, function (next) {
+  this.populate({
+    path: "guides",
+    select: "-__v -passwordChangedAt",
+  });
   next();
 });
 
